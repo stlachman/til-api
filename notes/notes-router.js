67b854f4d9db06ec703fd65318df760f95b1383e
Seth Lachman <lachmanseth17@gmail.com>
2019-08-19 18:38:25 -0400

    Added middleware function for validating the creation of notes.

--- notes/notes-router.js
+++ notes/notes-router.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const Notes = require("./notes-model");
 
 // Create Note
-router.post("/", (req, res) => {
+router.post("/", validateNote, (req, res) => {
   const note = req.body;
   Notes.add(note)
     .then(newPost => {
@@ -24,5 +24,16 @@ router.get("/", (req, res) => {
       res.status(500).json({ message: "Error retrieving notes" });
     });
 });
+
+function validateNote(req, res, next) {
+  const note = req.body;
+  if (note && note.title && note.description && note.user_id) {
+    next();
+  } else {
+    res
+      .status(400)
+      .json({ message: "You must include a name, description, and user id" });
+  }
+}
 
 module.exports = router;
