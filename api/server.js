e8bd478d0c0ef2a7e15e9658722f5d2fc4581562
Seth Lachman <lachmanseth17@gmail.com>
2019-08-19 14:26:12 -0400

    Created notes router.

--- api/server.js
+++ api/server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const server = express();
+const cors = require("cors");
 
 server.use(express.json());
+server.use(cors());
+
+const notesRouter = require("../notes/notesRouter");
 
 server.get("/", (req, res) => {
   res.status(200).json({ message: "Hello World" });
 });
+
+server.use("/api/notes", notesRouter);
 
 module.exports = server;
